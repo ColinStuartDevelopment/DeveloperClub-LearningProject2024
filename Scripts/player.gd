91ac0dc0d5faf146c2029c98aa02c9eb67c0197c
Colinisok <51425066+ColinStuartDevelopment@users.noreply.github.com>
2024-01-11 08:17:54 -0800

    Player Movement Done
    inputs and player movement.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,20 @@
 extends CharacterBody2D
 
+@export var speed : float = 100.0
 
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	
+	if velocity != Vector2.ZERO:
+		velocity = velocity.normalized() * speed
+	
+	move_and_slide()
 
