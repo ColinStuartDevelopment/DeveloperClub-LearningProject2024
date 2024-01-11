f2832d9a85616f89b9d0f43d0a6fb949fa6dbefb
Colinisok <51425066+ColinStuartDevelopment@users.noreply.github.com>
2024-01-11 12:37:44 -0800

    Enemy and Enemy Pathfinding
    added enemy and simple pathfinding

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed : float = 100.0
+@export var speed : float = 120.0
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
---
+++ Scripts/world.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var player_score : float = 0
+var player_health : float = 5
+
+@onready var player_health_text = $PlayerGUI/PlayerHealth
+@onready var player_score_text = $PlayerGUI/PlayerScore
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player_health_text.text = str(player_health)
+	player_score_text.text = str(player_score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
