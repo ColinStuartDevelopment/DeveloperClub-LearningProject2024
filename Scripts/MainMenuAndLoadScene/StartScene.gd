d63003e209a182399b6bceb40036c714ffb2cdc8
Colinisok <51425066+ColinStuartDevelopment@users.noreply.github.com>
2024-01-25 14:44:11 -0800

    Start Menu Scene looks good

--- Scripts/MainMenuAndLoadScene/StartScene.gd
+++ Scripts/MainMenuAndLoadScene/StartScene.gd
@@ -1,11 +1,15 @@
 extends Control
 
 const loading_scene_path = "res://Scenes/LoadingScene/load_scene.tscn"
+@onready var margin_container = $MarginContainer
 
 
 #This is how you add keyboard/game pad selection
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
+	margin_container.visible = false
+
+
 
 
 func _on_start_button_pressed():
@@ -17,7 +21,10 @@ func _on_load_button_pressed():
 
 
 func _on_settings_button_pressed():
-	pass # Replace with function body.
+	if margin_container.visible:
+		margin_container.visible = false
+	elif !margin_container.visible:
+		margin_container.visible = true
 
 
 func _on_about_button_pressed():
@@ -26,3 +33,5 @@ func _on_about_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
