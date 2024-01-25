dd4a24409fb892457412222f1700bd75db4c162f
Colinisok <51425066+ColinStuartDevelopment@users.noreply.github.com>
2024-01-25 13:55:14 -0800

    Working Start Menu Options

--- Scripts/MainMenuAndLoadScene/StartScene.gd
+++ Scripts/MainMenuAndLoadScene/StartScene.gd
@@ -2,6 +2,27 @@ extends Control
 
 const loading_scene_path = "res://Scenes/LoadingScene/load_scene.tscn"
 
-func _on_load_button_pressed() -> void:
+
+#This is how you add keyboard/game pad selection
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file(loading_scene_path)
 
+
+func _on_load_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_about_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -8,23 +8,27 @@ var player_health : float = 5
 @onready var progress_bar_one = $PlayerGUI/ProgressBarOne
 var progress_bar_one_value : float = 0.0
 @onready var timer_to_break_free = $PlayerGUI/TimerToBreakFree
+var time_speed_up : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	time_speed_up = 1.0
 	update_text()
 
 func update_text():
 	player_health_text.text = str(player_health)
 	player_score_text.text = str(player_score)
+	time_speed_up = time_speed_up + (player_score / 120)
+	print(time_speed_up)
 
 func _physics_process(delta):
-	progress_bar_one_value = progress_bar_one_value + delta
+	progress_bar_one_value = progress_bar_one_value + delta * time_speed_up
 	update_progress_bar()
 
 func update_progress_bar():
 	progress_bar_one.value = progress_bar_one_value
 	if progress_bar_one.value >= progress_bar_one.max_value:
-		pass
+		get_tree().change_scene_to_file("res://Scenes/card_world.tscn")
 
 func progress_bar_completed():
 	pass
