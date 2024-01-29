84f4147a183270b40827f7c481ee8d1b34e7ea23
Colinisok <51425066+ColinStuartDevelopment@users.noreply.github.com>
2024-01-29 14:44:12 -0800

    Fixed cards

--- Scripts/CardWorld/card.gd
+++ Scripts/CardWorld/card.gd
@@ -1,7 +1,7 @@
 extends AnimatableBody2D
 
 # Car Sprites to load
-const s_spade = preload("res://Assets/CardAssets/Symbol-Spade.png")
+const s_club = preload("res://Assets/CardAssets/Symbol-Club.png")
 const s_heart = preload("res://Assets/CardAssets/Symbol-Heart.png")
 const s_diamond = preload("res://Assets/CardAssets/Symbol-Diamond.png")
 const s_clover = preload("res://Assets/CardAssets/Symbol-Clover.png")
@@ -17,20 +17,340 @@ var current_card : String = "x"
 var current_position : Vector2 = Vector2.ZERO
 var card_clickable : bool = false
 
+var has_card_updated : bool = false
 
 func _ready():
 	# current_card = Globals.card_selected
 	current_card = Globals.card_selected
-	print(current_card)
 
 func _process(delta):
 	if card_clickable:
 		if Input.is_action_pressed("left_mouse"):
 			position = get_global_mouse_position()
+		if Input.is_action_just_released("left_mouse"):
+			card_clickable = false
+	
+	if has_card_updated == false:
+		print_card_stats()
 
 func _on_control_mouse_entered():
 	card_clickable = true
 
-func _on_control_mouse_exited():
-	card_clickable = false
+# func _on_control_mouse_exited():
+	# card_clickable = false
 
+func print_card_stats():
+	print(current_card)
+	if current_card == "club1":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "A"
+		$Control/NumberBL.text = "A"
+		$Control/NumberMiddle.text = "A"
+	elif current_card == "club2":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "2"
+		$Control/NumberBL.text = "2"
+		$Control/NumberMiddle.text = "2"
+	elif current_card == "club3":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "3"
+		$Control/NumberBL.text = "3"
+		$Control/NumberMiddle.text = "3"
+	elif current_card == "club4":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "4"
+		$Control/NumberBL.text = "4"
+		$Control/NumberMiddle.text = "4"
+	elif current_card == "club5":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "5"
+		$Control/NumberBL.text = "5"
+		$Control/NumberMiddle.text = "5"
+	elif current_card == "club6":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "6"
+		$Control/NumberBL.text = "6"
+		$Control/NumberMiddle.text = "6"
+	elif current_card == "club7":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "7"
+		$Control/NumberBL.text = "7"
+		$Control/NumberMiddle.text = "7"
+	elif current_card == "club8":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "8"
+		$Control/NumberBL.text = "8"
+		$Control/NumberMiddle.text = "8"
+	elif current_card == "club9":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "9"
+		$Control/NumberBL.text = "9"
+		$Control/NumberMiddle.text = "9"
+	elif current_card == "club10":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "10"
+		$Control/NumberBL.text = "10"
+		$Control/NumberMiddle.text = "10"
+	elif current_card == "club11":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "J"
+		$Control/NumberBL.text = "J"
+		$Control/NumberMiddle.text = "J"
+	elif current_card == "club12":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "Q"
+		$Control/NumberBL.text = "Q"
+		$Control/NumberMiddle.text = "Q"
+	elif current_card == "club13":
+		$TypeSpriteTL.texture = s_club
+		$TypeSpriteBR.texture = s_club
+		$Control/NumberTR.text = "K"
+		$Control/NumberBL.text = "K"
+		$Control/NumberMiddle.text = "K"
+	if current_card == "clover1":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "A"
+		$Control/NumberBL.text = "A"
+		$Control/NumberMiddle.text = "A"
+	elif current_card == "clover2":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "2"
+		$Control/NumberBL.text = "2"
+		$Control/NumberMiddle.text = "2"
+	elif current_card == "clover3":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "3"
+		$Control/NumberBL.text = "3"
+		$Control/NumberMiddle.text = "3"
+	elif current_card == "clover4":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "4"
+		$Control/NumberBL.text = "4"
+		$Control/NumberMiddle.text = "4"
+	elif current_card == "clover5":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "5"
+		$Control/NumberBL.text = "5"
+		$Control/NumberMiddle.text = "5"
+	elif current_card == "clover6":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "6"
+		$Control/NumberBL.text = "6"
+		$Control/NumberMiddle.text = "6"
+	elif current_card == "clover7":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "7"
+		$Control/NumberBL.text = "7"
+		$Control/NumberMiddle.text = "7"
+	elif current_card == "clover8":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "8"
+		$Control/NumberBL.text = "8"
+		$Control/NumberMiddle.text = "8"
+	elif current_card == "clover9":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "9"
+		$Control/NumberBL.text = "9"
+		$Control/NumberMiddle.text = "9"
+	elif current_card == "clover10":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "10"
+		$Control/NumberBL.text = "10"
+		$Control/NumberMiddle.text = "10"
+	elif current_card == "clover11":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "J"
+		$Control/NumberBL.text = "J"
+		$Control/NumberMiddle.text = "J"
+	elif current_card == "clover12":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "Q"
+		$Control/NumberBL.text = "Q"
+		$Control/NumberMiddle.text = "Q"
+	elif current_card == "clover13":
+		$TypeSpriteTL.texture = s_clover
+		$TypeSpriteBR.texture = s_clover
+		$Control/NumberTR.text = "K"
+		$Control/NumberBL.text = "K"
+		$Control/NumberMiddle.text = "K"
+	if current_card == "heart1":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "A"
+		$Control/NumberBL.text = "A"
+		$Control/NumberMiddle.text = "A"
+	elif current_card == "heart2":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "2"
+		$Control/NumberBL.text = "2"
+		$Control/NumberMiddle.text = "2"
+	elif current_card == "heart3":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "3"
+		$Control/NumberBL.text = "3"
+		$Control/NumberMiddle.text = "3"
+	elif current_card == "heart4":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "4"
+		$Control/NumberBL.text = "4"
+		$Control/NumberMiddle.text = "4"
+	elif current_card == "heart5":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "5"
+		$Control/NumberBL.text = "5"
+		$Control/NumberMiddle.text = "5"
+	elif current_card == "heart6":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "6"
+		$Control/NumberBL.text = "6"
+		$Control/NumberMiddle.text = "6"
+	elif current_card == "heart7":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "7"
+		$Control/NumberBL.text = "7"
+		$Control/NumberMiddle.text = "7"
+	elif current_card == "heart8":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "8"
+		$Control/NumberBL.text = "8"
+		$Control/NumberMiddle.text = "8"
+	elif current_card == "heart9":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "9"
+		$Control/NumberBL.text = "9"
+		$Control/NumberMiddle.text = "9"
+	elif current_card == "heart10":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "10"
+		$Control/NumberBL.text = "10"
+		$Control/NumberMiddle.text = "10"
+	elif current_card == "heart11":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "J"
+		$Control/NumberBL.text = "J"
+		$Control/NumberMiddle.text = "J"
+	elif current_card == "heart12":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "Q"
+		$Control/NumberBL.text = "Q"
+		$Control/NumberMiddle.text = "Q"
+	elif current_card == "heart13":
+		$TypeSpriteTL.texture = s_heart
+		$TypeSpriteBR.texture = s_heart
+		$Control/NumberTR.text = "K"
+		$Control/NumberBL.text = "K"
+		$Control/NumberMiddle.text = "K"
+	if current_card == "diamond1":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "A"
+		$Control/NumberBL.text = "A"
+		$Control/NumberMiddle.text = "A"
+	elif current_card == "diamond2":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "2"
+		$Control/NumberBL.text = "2"
+		$Control/NumberMiddle.text = "2"
+	elif current_card == "diamond3":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "3"
+		$Control/NumberBL.text = "3"
+		$Control/NumberMiddle.text = "3"
+	elif current_card == "diamond4":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "4"
+		$Control/NumberBL.text = "4"
+		$Control/NumberMiddle.text = "4"
+	elif current_card == "diamond5":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "5"
+		$Control/NumberBL.text = "5"
+		$Control/NumberMiddle.text = "5"
+	elif current_card == "diamond6":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "6"
+		$Control/NumberBL.text = "6"
+		$Control/NumberMiddle.text = "6"
+	elif current_card == "diamond7":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "7"
+		$Control/NumberBL.text = "7"
+		$Control/NumberMiddle.text = "7"
+	elif current_card == "diamond8":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "8"
+		$Control/NumberBL.text = "8"
+		$Control/NumberMiddle.text = "8"
+	elif current_card == "diamond9":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "9"
+		$Control/NumberBL.text = "9"
+		$Control/NumberMiddle.text = "9"
+	elif current_card == "diamond10":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "10"
+		$Control/NumberBL.text = "10"
+		$Control/NumberMiddle.text = "10"
+	elif current_card == "diamond11":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "J"
+		$Control/NumberBL.text = "J"
+		$Control/NumberMiddle.text = "J"
+	elif current_card == "diamond12":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "Q"
+		$Control/NumberBL.text = "Q"
+		$Control/NumberMiddle.text = "Q"
+	elif current_card == "diamond13":
+		$TypeSpriteTL.texture = s_diamond
+		$TypeSpriteBR.texture = s_diamond
+		$Control/NumberTR.text = "K"
+		$Control/NumberBL.text = "K"
+		$Control/NumberMiddle.text = "K"
+	has_card_updated = true
--- Scripts/card_world.gd
+++ Scripts/card_world.gd
@@ -2,24 +2,31 @@ extends Node2D
 
 const CARD = preload("res://Scenes/CardWorld/card.tscn")
 
-var cards_in_deck : Array = ["club1", "club2", "club3", "club4", "club5", "club6", "club7", "club8", "club9", "club10", "club11", "club12", "club13", "spade1", "spade2", "spade3", "spade4", "spade5", "spade6", "spade7", "spade8", "spade9", "spade10", "spade11", "spade12", "spade13", "heart1", "heart2", "heart3", "heart4", "heart5", "heart6", "heart7", "heart8", "heart9", "heart10", "heart11", "heart12", "heart13", "diamond1", "diamond2", "diamond3", "diamond4", "diamond5", "diamond6", "diamond7", "diamond8", "diamond9", "diamond10", "diamond11", "diamond12", "diamond13"]
+var cards_in_deck : Array = ["club1", "club2", "club3", "club4", "club5", "club6", "club7", "club8", "club9", "club10", "club11", "club12", "club13", "clover1", "clover2", "clover3", "clover4", "clover5", "clover6", "clover7", "clover8", "clover9", "clover10", "clover11", "clover12", "clover13", "heart1", "heart2", "heart3", "heart4", "heart5", "heart6", "heart7", "heart8", "heart9", "heart10", "heart11", "heart12", "heart13", "diamond1", "diamond2", "diamond3", "diamond4", "diamond5", "diamond6", "diamond7", "diamond8", "diamond9", "diamond10", "diamond11", "diamond12", "diamond13"]
 var cards_in_deck_number : int = 51
 
 var cards_being_shuffled : Array = []
-var cards_being_shuffled_number : int = 0
+var cards_being_shuffled_number : int = -1
+var cards_being_shuffled_count : int = -1
 
 var cards_in_hand : Array = []
 var cards_in_hand_number : int = 0
 
 var card_exchange = "x"
 
+@onready var card_count_variable = $Control/CardCountVariable
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	shuffle_deck()
+	card_count_variable.text = str(cards_in_deck_number + 1)
 
 func shuffle_deck():
-	for i in 52:
+	
+	for i in cards_in_deck_number + 1:
+		print("Cards in Deck: " + str(cards_in_deck_number + 1))
+		print("Cards in Shuffle: " + str(cards_being_shuffled_count + 1))
 		# PGenerate a number to decide what card to pull
 		cards_being_shuffled_number = randi_range(0, cards_in_deck_number)
 		# Create a variable to hold a card and then pull it
@@ -30,16 +37,18 @@ func shuffle_deck():
 		cards_in_deck.remove_at(cards_being_shuffled_number)
 		# Reduce the number of cards in deck and add the number of cards being shuffled
 		cards_in_deck_number = cards_in_deck_number - 1
-		cards_being_shuffled_number = cards_being_shuffled_number + 1
+		cards_being_shuffled_count = cards_being_shuffled_count + 1
 	# place all cards that have been shuffled back into the cards in deck
 	cards_in_deck = cards_being_shuffled.duplicate()
-	cards_in_deck_number = cards_being_shuffled_number
+	cards_in_deck_number = cards_being_shuffled_count
+	print("Done Shuffling - Cards in Deck:" + str(cards_in_deck_number + 1))
 	# remove all cards from the shuffled place holder
-	for i in 52:
-		cards_being_shuffled.remove_at(cards_being_shuffled_number)
-		cards_being_shuffled_number = cards_being_shuffled_number - 1
-	print("Deck has been shuffled...")
-
+	for i in cards_being_shuffled_count + 1:
+		cards_being_shuffled.remove_at(cards_being_shuffled_count)
+		cards_being_shuffled_count = cards_being_shuffled_count - 1
+		print("Cards in Shuffle: " + str(cards_being_shuffled_count + 1))
+	print("*** *** Deck has been shuffled *** ***")
+	card_count_variable.text = str(cards_in_deck_number + 1)
 
 func _on_shuffle_deck_button_pressed():
 	shuffle_deck()
@@ -49,6 +58,7 @@ func _on_draw_card_button_pressed():
 	card_exchange = cards_in_deck[cards_in_deck_number]
 	cards_in_deck.remove_at(cards_in_deck_number)
 	cards_in_deck_number -= 1
+	card_count_variable.text = str(cards_in_deck_number + 1)
 	Globals.card_selected = card_exchange
 	var card_in_game = CARD.instantiate()
 	add_child(card_in_game)
